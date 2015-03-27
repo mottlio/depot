792b1e673a2e6a9a0c3ca16d61781a43d5301d78
Michal Mottl <michal.mottl@coleurope.eu>
2015-03-27 20:49:34 +0100

    Final touch to authenticating admins

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,8 @@ Depot::Application.routes.draw do
 
 
 
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
